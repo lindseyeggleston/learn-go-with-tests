acb629182a5cafd65966203e82801c207f1245f0
Lindsey Eggleston <leggleston@sovrn.com>
2019-11-29 17:48:43 -0700

    add default functionality to Hello

--- hello/hello.go
+++ hello/hello.go
@@ -5,9 +5,12 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name + "\n"
 }
 
 func main() {
-	fmt.Printf(Hello("world"))
+	fmt.Printf(Hello(""))
 }
